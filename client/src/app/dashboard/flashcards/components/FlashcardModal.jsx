36a6e60b1efe7c6cfca919df382bd3606f4e5400
Ronald Hardock <rhardock@my.centennialcollege.ca>
2025-03-31 04:05:11 -0400

    Support added for arrow keys and spacebar for the flashcard viewing navigation. Spacebar flips the card. Right and Left arrow keys go to the next and previous cards.

--- client/src/app/dashboard/flashcards/components/FlashcardModal.jsx
+++ client/src/app/dashboard/flashcards/components/FlashcardModal.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Modal } from '@/components/ui/Modal';
 import { Button } from '@/components/ui/Button';
 import { FiChevronLeft, FiChevronRight, FiRefreshCcw } from 'react-icons/fi';
 import ReactCardFlip from 'react-card-flip';
 
 export default function FlashcardModal({ isOpen, onClose, flashcardDeck }) {
+  if (!flashcardDeck || flashcardDeck.flashcards.length === 0) return null;
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isFlipped, setIsFlipped] = useState(false);
-
-  if (!flashcardDeck || flashcardDeck.flashcards.length === 0) return null;
 
   const flashcards = flashcardDeck.flashcards;
   const totalCards = flashcards.length;
@@ -19,13 +19,36 @@ export default function FlashcardModal({ isOpen, onClose, flashcardDeck }) {
 
   const handleNext = () => {
     setIsFlipped(false);
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalCards);
+    setTimeout(() => { // Add a small delay to avoid showing the flipped state    
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % totalCards);
+    }, 150);
   };
 
   const handlePrev = () => {
     setIsFlipped(false);
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + totalCards) % totalCards);
+    setTimeout(() => { // Add a small delay to avoid showing the flipped state    
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + totalCards) % totalCards);
+    }, 150);
   };
+
+  // handle arrow keys and space bar for flashcard navigation and flipping
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "ArrowRight") {
+        handleNext();
+      } else if (event.key === "ArrowLeft") {
+        handlePrev();
+      } else if (event.key === " ") {
+        event.preventDefault();
+        flipCard();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleNext, handlePrev, flipCard]); // Add dependencies here to avoid stale closure
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
